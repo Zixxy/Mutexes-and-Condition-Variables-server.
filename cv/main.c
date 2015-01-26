3033260b1ea79515a41a74d348ae14edf81535b9
Zixxy <sylwekklocek@gmail.com>
2015-01-27 00:31:57 +0100

    Setting up server.

--- cv/main.c
+++ cv/main.c
@@ -3,22 +3,29 @@
 
 #define true 1
 
-void sef_setcb_init_fresh(int type, sef_init_info_t *info){}
-void sef_setcb_init_restart(){}
+int callnr;
+endpoint_t who_e;
+
+static void sef_local_startup(void);
+static int sef_cb_init_fresh(int type, sef_init_info_t *info);
+static void sef_cb_signal_handler(int signo);
 
 int main(int argc, char *argv[]){
 	message m;
 
-	sef_local_startup();
-
+	env_setargs(argc, argv);
+    sef_local_startup();
+	
 	while(true){
 		int r;
 		int status;
-		if((r = sef_receive(ANY, &m, &status)) != 0) //tu ma byc OK
+		if((r = sef_receive(ANY, &m, &status)) != OK) //tu ma byc OK zapytac
 			printf("receive failed %d.\n", r);
-		else
+		else{
 			printf("success!\n");
-
+			break;
+		}
+		printf("CV received %d %d from %d\n", r, callnr, who_e);
 	}
 }
 
@@ -26,15 +33,22 @@ int main(int argc, char *argv[]){
 
 static void sef_local_startup()
 {
-  /* Register init callbacks. */
-  sef_setcb_init_fresh(int type, sef_init_info_t *info); // this is in ipc main CHECK IT OUT
-  sef_setcb_init_restart(sef_cb_init_fresh);
-
-  /* No live update support for now. */
-
-  /* Register signal callbacks. */
-  sef_setcb_signal_handler(sef_cb_signal_handler);
-
-  /* Let SEF perform startup. */
-  sef_startup();
+    /* Register init callbacks. */
+    sef_setcb_init_fresh(sef_cb_init_fresh);
+    sef_setcb_init_restart(sef_cb_init_fresh);
+ 
+    /* Register signal callbacks. */
+    sef_setcb_signal_handler(sef_cb_signal_handler);
+    /* Let SEF perform startup. */
+    sef_startup();
+}
+ 
+static int sef_cb_init_fresh(int UNUSED(type), sef_init_info_t *UNUSED(info))
+{
+    return OK;
+}
+ 
+static void sef_cb_signal_handler(int signo)
+{
+    if (signo != SIGTERM) return;
 }
