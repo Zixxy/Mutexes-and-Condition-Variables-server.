391c9c4af5c3518fbc10a3e6ed408fcea602555b
Zixxy <sylwekklocek@gmail.com>
2015-01-30 18:40:02 +0100

    unlock_mutex()

--- cv/mutexes.c
+++ cv/mutexes.c
@@ -6,9 +6,10 @@
 
 #define SUCCESS 0
 #define WAIT 1
+#define IT_IS_NOT_YOURS -1
 
 struct Pender {
-	Pender* next_pender;
+	Pender* next_pending;
 	endpoint_t who;
 };
 
@@ -32,22 +33,22 @@ int create_mutexes(){
 
 int try_reservate(int which, endpoint_t who, int number){
 	if(reservations[which].who_has == NOBODY_HAS){
-		reservations[which].
+		reservations[which].number = number;
 		reservations[which].who_has = who;
 		return SUCCESS;
 	}
 	else{
 		Pender* new_pender = new Pender();
-		new_pender.next_pender = NULL;
+		new_pender.next_pending = NULL;
 		new_pender.who = who;
 
 		if(reservations[which].last_pending == NULL){
-			reservations[which].next_pender = new_pender;
+			reservations[which].next_pending = new_pender;
 			reservations[which].last_pending = new_pender;
 			return WAIT;
 		}
 		else{
-			reservations[which].last_pending -> next_pender = new_pender;
+			reservations[which].last_pending -> next_pending = new_pender;
 			reservations[which].last_pending = new_pender;
 			return WAIT;
 		}
@@ -68,4 +69,37 @@ int lock_mutex(int number, endpoint_t who){
 	return result = try_reservate(first_not_used, who, number);
 }
 
+int unlock_mutex(int number, endpoint_t who){
+	int which = -1;
+	for(int i = 0; i < POSSIBLE_RESERVATIONS; ++i){
+		if(reservations[i].number == number){
+			which = i;
+			break;
+		}
+	}
+	if(which == -1)
+		return IT_IS_NOT_YOURS;
 
+	if(reservations[which].who_has != who){
+		return IT_IS_NOT_YOURS;
+	}
+	else{
+		if(reservations[which].next_pending == NULL){
+			reservations[which].who_has = NOBODY_HAS;
+			return SUCCESS;
+		}
+		else{
+			reservations[which].who_has = reservations[which].next_pending.who;
+			//notify who_has!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			if(reservations[which].next_pending == reservations[which].last_pending){
+				reservations[which].next_pending = NULL;
+				reservations[which].last_pending = NULL;
+				return SUCCESS;
+			}
+			else{
+				reservations[which].next_pending = reservations[which].next_pending -> next_pending;
+				return SUCCESS;
+			}
+		}
+	}
+}
