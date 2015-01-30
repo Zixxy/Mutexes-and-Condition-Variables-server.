9c815850a8dde2cb0c195f769b0f6fc4683ebaa2
Zixxy <sylwekklocek@gmail.com>
2015-01-30 16:14:43 +0100

    Indludes.

---
+++ cv/constants.h
@@ -0,0 +1,4 @@
+#define LOCK_MUTEX 1
+#define UNLOCK_MUTEX 2
+
+#define m1_i1  m_u.m_m1.m1i1
---
+++ cv/includes.h
@@ -0,0 +1,36 @@
+#define _POSIX_SOURCE      1	/* tell headers to include POSIX stuff */
+#define _MINIX             1	/* tell headers to include MINIX stuff */
+#define _SYSTEM            1    /* get OK and negative error codes */
+
+#include <minix/callnr.h>
+#include <minix/com.h>
+#include <minix/config.h>
+#include <minix/ipc.h>
+#include <minix/endpoint.h>
+#include <minix/sysutil.h>
+#include <minix/const.h>
+#include <minix/type.h>
+#include <minix/syslib.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/mman.h>
+#include <machine/vm.h>
+#include <machine/vmparam.h>
+#include <sys/vm.h>
+
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+
+EXTERN int identifier;
+EXTERN endpoint_t who_e;
+EXTERN int call_type;
+EXTERN endpoint_t SELF_E;
+
