34bddef02028ea205ac4db1f11b88de261bbff81
Zixxy <sylwekklocek@gmail.com>
2015-02-04 11:50:39 +0100

    2 simple syscalls

---
+++ syscalls/cs_unclock.c
@@ -0,0 +1,13 @@
+#include <sys/cdefs.h>
+
+#define m1_i1  m_u.m_m1.m1i1
+
+int cs_lock(int mutex_id){
+	int CV;
+    endpoint_t endpoint = minix_rs_lookup("cv", &CV);
+    message msg;
+    msg.m_type = 2;
+    msg.m1_i1 = mutex_id;
+
+    return _syscall(val, endpoint, &msg);
+}
